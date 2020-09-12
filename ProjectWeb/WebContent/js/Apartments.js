b16f510f5548a7a999d1f9dbb117c421e5568a5a
bojanaat <tekicbojana98@gmail.com>
2020-09-12 15:20:54 +0200

    Kreiran dinamicki pregled apartmana

--- ProjectWeb/WebContent/js/Apartments.js
+++ ProjectWeb/WebContent/js/Apartments.js
@@ -26,10 +26,11 @@ function load() {
 		method: "GET",
 		datatype : "application/json"
 	}).success(function(data) {
-		let dynamicHTML = "<<thead><tr><th scope=\"col\">Id</th><th scope=\"col\">Name</th><th scope=\"col\">Type</th><th scope=\"col\">Number of Rooms</th>" +
+		console.log(data);
+		let dynamicHTML = "<<thead><tr><th scope=\"col\">Id</th><th scope=\"col\">Name</th><th scope=\"col\">Type</th>" +
 				"<th scope=\"col\">Number of Rooms</th><th scope=\"col\">Number of guests</th><th scope=\"col\">Price for night</th>" + 
 				"<th scope=\"col\">Time for check in</th><th scope=\"col\">Time for check out</th><th scope=\"col\">Location</th>"+ 
-				"<th scope=\"col\">Comments</th><th class=\"variant1\">Go to profile</th><th class=\"variant2\">Delete</th></tr></thead><tbody>";
+				"<th class=\"variant1\">Go to profile</th><th class=\"variant2\">Delete</th></tr></thead><tbody>";
 				
 				
 	    let i;
@@ -59,21 +60,20 @@ function load() {
 			dynamicHTML+="<td>";
 			dynamicHTML+=data[i].timeForCheckOut;
 			dynamicHTML+="</td>";
-			dynamicHTML+="<td></td>";
-			dynamicHTML+="<td></td>";
-			dynamicHTML+="<td></td>";
-			dynamicHTML+="<td></td>";
+			dynamicHTML+="<td>";
+			dynamicHTML+= data[i].location.address.city + " " + data[i].location.address.street;
+			dynamicHTML+="</td>";
+			dynamicHTML+="<td><a href=\"UpdateApartment.html?id=" + data[i].id + "\" >Update</a></td>";
+			dynamicHTML+="<td><a href=\"Apartment.html?id=" + data[i].id + "\" >Profile</a></td>";
+			dynamicHTML+="<td><button onclick=\"delete(" + data[i].id + ")\">Delete<button></td>";
 			
-			//dynamicHTML+="<td>";
-			//dynamicHTML+=data[i].location.address.street + " " + location.address.city;
-			//dynamicHTML+="</td>";
 			
 			dynamicHTML+="</tr>";
 		}
 		
 		dynamicHTML+="</tbody>";
-		
-		$("#tab1").append(dynamicHTML);
+		$("#tab2").empty();
+		$("#tab2").append(dynamicHTML);
 	});
 		
 }
